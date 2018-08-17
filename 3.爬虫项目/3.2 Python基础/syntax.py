5b9e38dff5630caf8c7aee691e4841263ac36b89
LittleHeap <418761605@qq.com>
2018-08-17 21:29:27 +0800

    update

--- 3.爬虫项目/3.2 Python基础/syntax.py
+++ 3.爬虫项目/3.2 Python基础/syntax.py
@@ -1,23 +1,19 @@
 a = 1234
-print(a)
+print(a)  # 1234
 a = 'abcd'
-print(a)
+print(a)  # abcd
 
 try:
-    print(b)
+    print(a)  # abcd
 except Exception as e:
     print(e)
 
-a = [1, 2, 3 , 4]
+a = [1, 2, 3, 4]
+
 
 def func(a):
     a[0] = 2
 
-func(a)
-print(a)
 
-try:
-    # Python 2.x 支持
-    print 100, 200, 300
-except Exception as e:
-    print(e)
+func(a)
+print(a)  # [2, 2, 3, 4]
--- 3.爬虫项目/3.2 Python基础/threads.py
+++ 3.爬虫项目/3.2 Python基础/threads.py
@@ -1,15 +1,28 @@
 import threading
 
+
 def thread_func(x):
-    # 自己加sleep和其它复杂操作看效果
     print('%d\n' % (x * 100))
+
 
 threads = []
 for i in range(5):
-    threads.append(threading.Thread(target = thread_func, args = (100, )))
+    threads.append(threading.Thread(target=thread_func, args=(100,)))
 
 for thread in threads:
     thread.start()
 
 for thread in threads:
     thread.join()
+
+'''
+    10000
+    
+    10000
+    
+    10000
+    
+    10000
+    
+    10000
+'''
