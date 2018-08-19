143135ab392db83da37fec4893657cb6ed168ae4
LittleHeap <418761605@qq.com>
2018-08-19 21:41:12 +0800

    update

--- 3.爬虫项目/3.4 爬虫基础/douban_login_up.py
+++ 3.爬虫项目/3.4 爬虫基础/douban_login_up.py
@@ -3,22 +3,28 @@ import html5lib
 import re
 from bs4 import BeautifulSoup
 
-
 s = requests.Session()
+
 url_login = 'http://accounts.douban.com/login'
 
 formdata = {
-    'redir':'https://www.douban.com',
+    'redir': 'https://www.douban.com',
     'form_email': 't.t.panda@hotmail.com',
     'form_password': 'tp65536!',
     'login': u'登陆'
 }
-headers = {'user-agent': 'Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.157 Safari/537.36'}
 
-r = s.post(url_login, data = formdata, headers = headers)
+headers = {
+    'user-agent': 'Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.157 Safari/537.36'}
+
+r = s.post(url_login, data=formdata, headers=headers)
+
 content = r.text
+
 soup = BeautifulSoup(content, 'html5lib')
-captcha = soup.find('img', id = 'captcha_image')
+
+captcha = soup.find('img', id='captcha_image')
+
 if captcha:
     captcha_url = captcha['src']
     re_captcha_id = r'<input type="hidden" name="captcha-id" value="(.*?)"/'
@@ -28,7 +34,7 @@ if captcha:
     captcha_text = input('Please input the captcha:')
     formdata['captcha-solution'] = captcha_text
     formdata['captcha-id'] = captcha_id
-    r = s.post(url_login, data = formdata, headers = headers)
-with open('contacts.txt', 'w+', encoding = 'utf-8') as f:
-    f.write(r.text)
+    r = s.post(url_login, data=formdata, headers=headers)
 
+with open('contacts.txt', 'w+', encoding='utf-8') as f:
+    f.write(r.text)
